42cf1ecd32f4b8246b63a8e1ed212b2a32536a1f
Juhi Ramesh Joshi <85553000+juhijoshi-coder@users.noreply.github.com>
2023-09-02 10:23:16 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+let buttons = document.querySelectorAll('.btn');
+let screen  = document.querySelector('.screen');
+let clear = document.querySelector('.btn-clear');
+let equal = document.querySelector('.btn-equal');
+
+buttons.forEach(function(button){
+    button.addEventListener('click', function(e){
+        let value = e.target.dataset.num;
+        screen.value += value
+    })
+})
+
+clear.addEventListener('click',function(e){
+    screen.value = '';
+})
+
+equal.addEventListener('click', function(e){
+    if(screen.value ===''){
+        screen.value = '';
+    }else{
+        let answer = eval(screen.value)
+        screen.value = answer
+    }
+})
